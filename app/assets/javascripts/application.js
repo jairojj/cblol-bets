90f9189e619cce1e5a0a1b70487284e59f625724
Jairo Junior <obelisco80@hotmail.com>
2017-02-01 15:56:43 +0000

    new home/layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,3 +37,30 @@ $(function(){
   });  
 
 })
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidenav when clicking the menu icon
+
+function w3_open() {
+  var mySidenav = document.getElementById("mySidenav");
+  
+    if (mySidenav.style.display === 'block') {
+        mySidenav.style.display = 'none';
+    } else {
+        mySidenav.style.display = 'block';
+    }
+}
+
+// Close the sidenav with the close button
+function w3_close() {
+    var mySidenav = document.getElementById("mySidenav");
+    mySidenav.style.display = "none";
+}
