433f25ed1496c53f2eb56f1a5253aa8a4c56a5b6
Jairo Junior <obelisco80@hotmail.com>
2017-02-03 16:05:47 +0000

    mudanças no script de apostas

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -7,15 +7,17 @@ class Bet < ApplicationRecord
   validates :valor, numericality: { :greater_than_or_equal_to => 200 }
   
   def self.verificar_vencedores
-    apostas_vencedoras = Array.new
-    vencedores = Array.new
-    coin_total = 0
-    coin_separado = 0
     
-    @jogos = Jogo.where(status: true)
+    @jogos = Jogo.where(status: true).last(4)
     
     @jogos.each do |jogo|
       
+      apostas_vencedoras = 0
+      vencedores = Array.new
+      perdedores = 0
+      coin_apostado = 0
+      coin_final = 0
+
       @apostas = Bet.where(jogo_id: jogo.id)
       
       @apostas.each do |aposta|
@@ -23,26 +25,26 @@ class Bet < ApplicationRecord
         if aposta.resultado == jogo.resultado
           vencedores << aposta.user_id
           apostas_vencedoras << aposta.id
-          coin_total = coin_total + aposta.valor
-          puts "coin_total " + coin_total.to_s
+        else
+          perdedores = perdedores + aposta.valor
         end
         
       end
       
+        puts "vencedores " + vencedores.length.to_s
+        
+        vencedores.each do |vencedor|
+          coin_atual = User.find(vencedor).coin
+          coin_apostado = Bet.where(user_id: vencedor, jogo_id: jogo.id).take
+          puts "coin_atual " + coin_atual.to_s
+          coin_final = coin_atual + coin_apostado.valor + ( perdedores / vencedores.length )
+          puts "coin final: " + coin_final.to_s
+          User.update(vencedor, :coin => coin_final)
+          aposta_vencedora = Bet.where(jogo_id: jogo.id, user_id: vencedor).take
+          Bet.update(aposta_vencedora.id, :ganhou_qnt =>  coin_apostado.valor + ( perdedores / vencedores.length ) )
+        end
+    
     end
-    
-    coin_separado = coin_total / vencedores.length
-    puts "vencedores " + vencedores.length.to_s
-    
-    vencedores.each do |vencedor|
-      coin_atual = User.find(vencedor).coin
-      puts "coin_atual " + coin_atual.to_s
-      User.update(vencedor, :coin => coin_atual + coin_separado)
-    end
-    
-    apostas_vencedoras.each do |aposta_vencedora|
-      Bet.update(aposta_vencedora, :ganhou_qnt => coin_separado)
-    end
-    
+  
   end
 end
